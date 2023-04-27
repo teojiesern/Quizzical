428b4582711165a42a5925bdcbfd0b6ebe721e5e
teojiesern <teojiesern42@gmail.com>
2023-04-28 00:46:55 +0800

    finished landing page styles

---
+++ src/components/Blobs.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Blobs(){
+    return (
+        <div>
+            <img src="/assets/upperBlob.png" className="upperBlob"></img>
+            <img src="/assets/lowerBlob.png" className="lowerBlob"></img>
+        </div>
+    )
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Blobs from "../components/blobs";
 
 export default function LandingPage(){
-    
+    return (
+    <div className="landing-page">
+        <Blobs />
+        <h1 className="title">Quizzical</h1>
+        <p className="description">Fun Quiz awaits!!!</p>
+        <button className="start-btn">Start Quiz</button>
+    </div>
+    )
 }
